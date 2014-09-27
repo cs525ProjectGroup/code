f8e61bfa4c3007a1a730630443ea79376ed38739
Yangyang Xie <yxie23@hawk.iit.edu>
2014-09-27 13:44:51 -0500

    write the method :createPageFile;

--- assign1/extraCredits/calcAvgScore/calcAvgScore/main.c
+++ assign1/extraCredits/calcAvgScore/calcAvgScore/main.c
@@ -6,23 +6,13 @@
 //  Copyright (c) 2014 xieyangyang. All rights reserved.
 //
 
-#include <stdio.h>
-＃include<stdio.h>
-int main(int argc, const char * argv[]) {
-
-
-    int argc;
-    char *argv[];
-    char ch;
-    FILE *fp;
-    int i;
-    if((fp=fopen(argv[1],"r"))==NULL) /* 打开一个由argv[1]所指的文件*/
-    {
-        printf("not open");
-        exit(0);
-    }
-    while ((ch=fgetc(fp))!=EOF) /* 从文件读一字符，显示到屏幕*/
-        putchar(ch);
-    fclose(fp);
-}
+#include<stdio.h>
+#include <malloc/malloc.h>
+int main(void)
+{
+    FILE * fp;
+    char str[11];
+    fp=fopen("/Users/xieyangyang/Desktop/student.cvs","r");
+    fread(str, sizeof(char), 10, fp);
+    printf("%s",str);
 }
