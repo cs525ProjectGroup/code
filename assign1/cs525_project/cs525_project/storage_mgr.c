932c6d7ecdfcc54c84575ab0ac6443562ccb8805
Yangyang Xie <yxie23@hawk.iit.edu>
2014-09-28 14:21:06 -0500

    creat and open file methods have been finished.

--- assign1/cs525_project/cs525_project/storage_mgr.c
+++ assign1/cs525_project/cs525_project/storage_mgr.c
@@ -5,15 +5,15 @@
 //  Created by xieyangyang on 9/24/14.
 //
 //
-
-#include "storage_mgr.h"
 #include <stdio.h>
-
 #include <stdlib.h>
 #include "dberror.h"
+#include "storage_mgr.h"
+
+
 void initStorageManager (void)
 {
-    return ;
+    
 }
 
 
@@ -21,26 +21,45 @@ void initStorageManager (void)
 //This method should fill this single page with '\0' bytes.
 RC createPageFile (char *fileName)
 {
-    FILE *pf;
-    char* buff = (char*)calloc(PAGE_SIZE,sizeof(char));
+    FILE *pf=NULL;
+    char* buff = (char *)calloc(PAGE_SIZE,sizeof(char));
     pf = fopen(fileName,"wb");
-    if(pf==NULL)
-        return RC_FILE_NOT_FOUND;
-    else
-    {
-        if(fread(buff, sizeof(char), PAGE_SIZE, pf)==0)
-            return RC_WRITE_FAILED;
+    int returnV = -1;
+        if(fwrite(buff, sizeof(char), PAGE_SIZE, pf)==0)
+            returnV=RC_WRITE_FAILED;
         else
-            return RC_OK;
-    }
+            returnV = RC_OK;
+    free(buff);
+    fclose(pf);
+    return returnV;
+    
+    
 }
 
 /*Opens an existing page file. Should return RC_FILE_NOT_FOUND if the file does not exist. The second parameter is an existing file handle. If opening the file is successful, then the fields of this file handle should be initialized with the information about the opened file. For instance, you would have to read the total number of pages that are stored in the file from disk.
  */
 RC openPageFile (char *fileName, SM_FileHandle *fHandle)
 {
+    FILE *pf=NULL;
+    int len;
+    pf=fopen(fileName, "rb");
+    int returnV;
+    if(pf==NULL)
+       returnV = RC_FILE_NOT_FOUND;
+    else
+    {
+        fseek(pf,0L,SEEK_END);// figure out the tail position of the file, and let fp points to this position.
+        len=ftell(pf)+1; //length from the tail to the header of the page file = bytes of the page file.
+        fHandle ->fileName = fileName;
+        fHandle ->totalNumPages = len/PAGE_SIZE;
+        fHandle ->curPagePos =0;
+        fHandle ->mgmtInfo = pf;
+        returnV=RC_OK;
+    }
     
+
 }
+/*Close an open page file or destroy (delete) a page file.*/
 RC closePageFile (SM_FileHandle *fHandle)
 {
     
