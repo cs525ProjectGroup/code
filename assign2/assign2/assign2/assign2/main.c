28cb6005f639e6d0b5d0d307cf248d609c18ca4c
Yangyang Xie <yxie23@hawk.iit.edu>
2014-10-23 12:23:52 -0500

    assignment1

---
+++ assign2/assign2/assign2/assign2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  assign2
+//
+//  Created by xieyangyang on 10/23/14.
+//  Copyright (c) 2014 xieyangyang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
